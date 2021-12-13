7aea882a689c806f32fb52292dacc4077039fef5
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2021-12-13 16:17:37 +0100

    Day 13

--- 2021/day12.ml
+++ 2021/day12.ml
@@ -1,27 +1,26 @@
 module PathSet = Set.Make(struct type t = string list let compare = compare end)
 
-let rec pathset_fold f x = let y, x' = f x in if x' = PathSet.empty then y else PathSet.union y (pathset_fold f x')
-
 let is_big node = node.[0] < 'a'
+
+let rec pathset_unfold f x = let y, x' = f x in if x' = PathSet.empty then y else y + (pathset_unfold f x')
 
 let next g path graph =
     let last = List.hd path in
-    if last = "end" then (PathSet.singleton path, PathSet.empty) else
+    if last = "end" then (1, PathSet.empty) else
         graph
         |> List.filter_map (fun (a, b) ->  if a = last then Some b else if b = last then Some a else None)
         |> List.filter_map (fun fresh -> if is_big fresh || not (List.mem fresh path) || g fresh path then Some (fresh :: path) else None)
         |> List.partition (fun path -> List.hd path = "end")
-        |> fun (s, t) -> PathSet.of_list s, PathSet.of_list t
+        |> fun (s, t) -> List.length s, PathSet.of_list t
 
-let step g graph set = PathSet.fold (fun path (s, t) -> let s', t' = next g path graph in PathSet.union s s', PathSet.union t t') set (PathSet.empty, PathSet.empty)
+let step g graph set = PathSet.fold (fun path (s, t) -> let s', t' = next g path graph in s + s', PathSet.union t t') set (0, PathSet.empty) 
 
 let f g path =
     path
     |> open_in
     |> Seq.unfold Misc.input_line
     |> Seq.fold_left (fun u str -> str |> String.split_on_char '-' |> function [a; b] -> (a, b) :: u | _ -> failwith "") []
-    |> fun graph -> pathset_fold (step g graph) (PathSet.singleton ["start"])
-    |> PathSet.cardinal
+    |> fun graph -> pathset_unfold (step g graph) (PathSet.singleton ["start"])
 
 let _ = Misc.process (f (fun _ _ -> false)) 10
 
--- 2021/day13.ml
+++ 2021/day13.ml
@@ -1,13 +1,27 @@
+let fold sym x = if x < sym then x else 2 * sym - x
+
+let fold (dir, sym) (x, y) = if dir then (fold sym x, y) else (x, fold sym y)
+
+module PairSet = Set.Make(struct type t = int * int let compare = compare end)
+
+let grid h w u = List.fold_left (fun a (i, j) -> let a = Array.copy a in Fun.const a (a.(j).(i) <- true)) (Array.make_matrix h w false) u
+let print = Array.iter (fun row -> Array.iter (fun cell -> print_char (if cell then '#' else '.')) row; print_newline ())
+
+let read_input path =
+    let f c cin = cin |> Seq.unfold (fun cin -> Option.bind (Misc.input_line cin) (fun (str, cin) -> str |> String.split_on_char c |> function [a; b] -> Some ((a, b), cin) | _ -> None)) in
+    let cin = path |> open_in in 
+    let data = cin |> f ',' |> Seq.map (fun (a, b) -> int_of_string a, int_of_string b)|> PairSet.of_seq in
+    let syms = cin |> f '=' |> Seq.map (fun (a, b) -> a.[String.length a - 1] = 'x', int_of_string b) |> List.of_seq in
+    (data, syms)
+
 let f path =
-    let data = path
-    |> open_in
-    |> Seq.unfold Misc.input_line
-    |> List.of_seq 
-    |> List.rev in
-    let y, data = data |> List.hd |> fun str -> String.sub str 13 (String.length str - 13) |> int_of_string, List.tl data in
-    let x, data = data |> List.hd |> fun str -> String.sub str 13 (String.length str - 13) |> int_of_string, List.tl data in
-    let _ = List.tl data in
-    failwith "TODO"
+    let data, syms = read_input path in
+    PairSet.fold (fun (x, y) set -> PairSet.add (fold (List.hd syms) (x, y)) set) data PairSet.empty
 
+let _ = Misc.process (fun path -> path |> f |> PairSet.cardinal) 17
 
-let _ = Misc.process f 17
+let f path =
+    let data, syms = read_input path in
+    List.fold_left (fun set (vert, sym) -> PairSet.fold (fun (x, y) set -> PairSet.add (fold (vert, sym) (x, y)) set) set PairSet.empty) data syms
+
+let _ = "day13.data" |> f |> PairSet.elements |> grid 8 64 |> print
