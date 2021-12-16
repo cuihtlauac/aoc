a6d2a21882e36bd8db2c2109a5cc5ee797e81c1b
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2021-12-16 09:20:01 +0100

    Update

--- 2021/day15.ml
+++ 2021/day15.ml
@@ -12,25 +12,29 @@ let def f x y z = if x = z then y else f z
 module Cache = Set.Make(struct type t = (int * int) * int let compare (_, a) (_, b) = compare a b end)
 
 let rec loop mat visited border =
-    let (i, j), len = try Cache.min_elt border with | e -> Printf.printf "bang!"; raise e in
+    let (i, j), len = Cache.min_elt border in
+    Printf.eprintf "visit %i %i (visited: %i) (border: %i) (trip: %i) \n" i j (List.length visited) (Cache.cardinal border) len;
     let border = Cache.remove ((i, j), len) border in
-    Printf.printf "%i %i: \n" i j;
+    let visited = ((i, j), len) :: visited in
     if i = Array.length mat - 1 && j = Array.length mat - 1 then
         len
-    else
-        let visited = ((i, j), len) :: visited in
-        let g pos border = match (try Some mat.(i).(j) with _ -> None), List.assoc_opt pos visited, (Cache.find_last_opt (fun (x, _) -> x = pos) border) with
-        | None,   _,      _ -> border (* not in mat *)
-        | _,      Some _, _  -> border (* already visited *)
-        | Some x, _,      Some (_, len') when len + x < len' -> border |> Cache.remove (pos, len') |> Cache.add (pos, len + x)
-        | Some _, None,   Some (_, _) -> border (* not better *)
-        | Some x, _,      None -> border |> Cache.add (pos, len + x) 
-    in
-        let border = g (i, j - 1) border in
-        let border = g (i, j + 1) border in
-        let border = g (i - 1, j) border in
-        let border = g (i + 1, j) border in
-        loop mat visited border
+    else 
+
+        let g pos border = 
+            match (try Some mat.(fst pos ).(snd pos) with Invalid_argument _ -> None), List.assoc_opt pos visited, Option.map snd (Cache.find_last_opt (fun (x, _) -> x = pos) border) with
+            | None, _, _ -> (Printf.eprintf "off    |" ; border)
+            | _, Some _, _ -> (Printf.eprintf "visited|" ; border)
+            | Some x,  _, Some len' when len + x < len' -> (Printf.eprintf "update %i %i|" len' (len + x); border |> Cache.remove (pos, len') |> Cache.add (pos, len + x))
+            | _,  _, Some _                        -> (Printf.eprintf      "discard|"; border (* not better *))
+            | Some x,  _, None -> (Printf.eprintf "insert %i@[%i,%i]|" (len + x) (fst pos) (snd pos); Cache.add (pos, len + x) border)
+            in
+                Printf.eprintf "west :";let border = g (i, j - 1) border in
+                Printf.eprintf "east :";let border = g (i, j + 1) border in
+                Printf.eprintf "north:";let border = g (i - 1, j) border in
+                Printf.eprintf "south:";let border = g (i + 1, j) border in
+            Printf.eprintf "#%i#\n" (Cache.cardinal border);
+            loop mat visited border
+    
 
 let f path =
     let mat = 
